a3749ed1917b1f8adb023918b4303fc64c697f32
ramyyasser21102 <ramy.yasser21@tutamail.com>
2026-06-10 07:09:29 +0300

    Added config module, typeormmodule, user, health modules. Added validation pipes in main.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { HealthModule } from './health/health.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from './user/user.entity';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+        type: 'better-sqlite3',
+        database: configService.get<string>('DATABASE_URL'),
+        entities: [User],
+        synchronize: configService.get('NODE_ENV') !== 'production',
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import 'reflect-metadata';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const validationPipe = new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  });
+  app.useGlobalPipes(validationPipe);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`Application is running on port ${port}`);
 }
 
-bootstrap()
-  .then(() => {
-    console.log('Application is running on port 3000');
-  })
-  .catch((error) => {
-    console.error('Error starting the application:', error);
-  });
+bootstrap().catch((error) => {
+  console.error('Error starting the application:', error);
+  throw error;
+});
